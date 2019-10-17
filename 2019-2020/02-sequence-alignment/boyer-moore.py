7e2b8da2282314b102b0179f6d47bce4bee4055b
James Hawley <jamesrichardhawley@gmail.com>
2019-10-17 18:22:41 -0400

    Checking in code from Ben Langmead

--- 2019-2020/02-sequence-alignment/boyer-moore.py
+++ 2019-2020/02-sequence-alignment/boyer-moore.py
@@ -1,55 +1,187 @@
-def boyer_moore(s, t, pre_s=None):
-    if pre_s is None:
-        pre_s = preprocess(s)                                           # preprocess s if needed
-    counter = 0                                                         # count number of comparisons
-    i = 0
+"""
+From Ben Langmead
+https://j.mp/CG_BoyerMoore
+"""
+
+import string
+
+def boyer_moore(s, p_bm, t):
+    """ Do Boyer-Moore matching """
+    i = 0                                                       # start position of 
     occurrences = []
-    while i < len(t) - len(s) + 1:                                      # loop over start positions for s
+    while i < len(t) - len(s) + 1:
         shift = 1
         mismatched = False
-        for j in range(start=len(s), stop=-1, step=-1):                 # start counting from right (Longer Skip Rule)
-            counter += 1
-            if s[j] != t[i + j]:                                        # if mismatch
-                shift_bad_char = pre_s.bad_char(j, t[i + j])            # calculate skips due to Bad Character Rule
-                shift_good_suffix = pre_s.good_suffix(j)                # calculate skips due to Good Suffix Rule
-                shift = max(shift, shift_bad_char, shift_good_suffix)   # skip as many as guaranteed
+        for j in range(len(s)-1, -1, -1):
+            if s[j] != t[i+j]:
+                skip_bc = p_bm.bad_character_rule(j, t[i+j])
+                skip_gs = p_bm.good_suffix_rule(j)
+                shift = max(shift, skip_bc, skip_gs)
                 mismatched = True
                 break
-        if not mismatched:                                              # store i if everything matches
+        if not mismatched:
             occurrences.append(i)
-            shift_good_suffix = pre_s.match_skip()                      # given it matched, calc how many to skip
-            shift = max(shift, shift_good_suffix)                       # skip as many as possible
+            skip_gs = p_bm.match_skip()
+            shift = max(shift, skip_gs)
         i += shift
-    return occurrences, counter
-
-
-def pre_bad_char(s, alphabet=['A', 'C', 'G', 'T']):
-    last_seen = {a: 0 for a in alphabet}                        # keep track of last time you've seen a character
-    skips = {a: [None] * len(s) for a in alphabet}              # skips to calculate
-    for i, a in enumerate(alphabet):                            # set first column to 0 since none to skip
-        if s[0] != a:
-            skips[a][0] = 0
-    for i, char in enumerate(s):                                # iterate over s
-        for a in alphabet:
-            if s[i] != a:
-                skips[a][i] = last_seen[a]                      # record how many times ago you saw this character
-        last_seen = {a: (j + 1) for a, j in last_seen.items()}  # update counts
-        last_seen[char] = 0                                     # except for the character you just saw
-    return skips
-
-def pre_good_suffix(s):
-    pass
-
-def print_bm(s, t):
-    print(t)
-    occurrences, counter = boyer_moore(s, t)
-    for i in occurrences:
-        print(' ' * i + s)
-    print(occurrences)
-    print(counter, 'comparisons')
-    print('')
     return occurrences
 
-pre_s = preprocess(s)                                               # preprocess skips on s
-print(preprocess('TCGCC'))
+def z_array(s):
+    """ Use Z algorithm (Gusfield theorem 1.4.1) to preprocess s """
+    assert len(s) > 1
+    z = [len(s)] + [0] * (len(s)-1)
+    # Initial comparison of s[1:] with prefix
+    for i in range(1, len(s)):
+        if s[i] == s[i-1]:
+            z[1] += 1
+        else:
+            break
+    r, l = 0, 0
+    if z[1] > 0:
+        r, l = z[1], 1
+    for k in range(2, len(s)):
+        assert z[k] == 0
+        if k > r:
+            # Case 1
+            for i in range(k, len(s)):
+                if s[i] == s[i-k]:
+                    z[k] += 1
+                else:
+                    break
+            r, l = k + z[k] - 1, k
+        else:
+            # Case 2
+            # Calculate length of beta
+            nbeta = r - k + 1
+            zkp = z[k - l]
+            if nbeta > zkp:
+                # Case 2a: Zkp wins
+                z[k] = zkp
+            else:
+                # Case 2b: Compare characters just past r
+                nmatch = 0
+                for i in range(r+1, len(s)):
+                    if s[i] == s[i - k]:
+                        nmatch += 1
+                    else:
+                        break
+                l, r = k, r + nmatch
+                z[k] = r - k + 1
+    return z
 
+
+def n_array(s):
+    """ Compile the N array (Gusfield theorem 2.2.2) from the Z array """
+    return z_array(s[::-1])[::-1]
+
+
+def big_l_prime_array(p, n):
+    """ Compile L' array (Gusfield theorem 2.2.2) using p and N array.
+        L'[i] = largest index j less than n such that N[j] = |P[i:]| """
+    lp = [0] * len(p)
+    for j in range(len(p)-1):
+        i = len(p) - n[j]
+        if i < len(p):
+            lp[i] = j + 1
+    return lp
+
+
+def big_l_array(p, lp):
+    """ Compile L array (Gusfield theorem 2.2.2) using p and L' array.
+        L[i] = largest index j less than n such that N[j] >= |P[i:]| """
+    l = [0] * len(p)
+    l[1] = lp[1]
+    for i in range(2, len(p)):
+        l[i] = max(l[i-1], lp[i])
+    return l
+
+
+def small_l_prime_array(n):
+    """ Compile lp' array (Gusfield theorem 2.2.4) using N array. """
+    small_lp = [0] * len(n)
+    for i in range(len(n)):
+        if n[i] == i+1:  # prefix matching a suffix
+            small_lp[len(n)-i-1] = i+1
+    for i in range(len(n)-2, -1, -1):  # "smear" them out to the left
+        if small_lp[i] == 0:
+            small_lp[i] = small_lp[i+1]
+    return small_lp
+
+
+def good_suffix_table(p):
+    """ Return tables needed to apply good suffix rule. """
+    n = n_array(p)
+    lp = big_l_prime_array(p, n)
+    return lp, big_l_array(p, lp), small_l_prime_array(n)
+
+
+def good_suffix_mismatch(i, big_l_prime, small_l_prime):
+    """ Given a mismatch at offset i, and given L/L' and l' arrays,
+        return amount to shift as determined by good suffix rule. """
+    length = len(big_l_prime)
+    assert i < length
+    if i == length - 1:
+        return 0
+    i += 1  # i points to leftmost matching position of P
+    if big_l_prime[i] > 0:
+        return length - big_l_prime[i]
+    return length - small_l_prime[i]
+
+
+def good_suffix_match(small_l_prime):
+    """ Given a full match of P to T, return amount to shift as
+        determined by good suffix rule. """
+    return len(small_l_prime) - small_l_prime[1]
+
+
+def dense_bad_char_tab(p, amap):
+    """ Given pattern string and list with ordered alphabet characters, create
+        and return a dense bad character table.  Table is indexed by offset
+        then by character. """
+    tab = []
+    nxt = [0] * len(amap)
+    for i in range(0, len(p)):
+        c = p[i]
+        assert c in amap
+        tab.append(nxt[:])
+        nxt[amap[c]] = i+1
+    return tab
+
+
+class BoyerMoore(object):
+    """ Encapsulates pattern and associated Boyer-Moore preprocessing. """
+    
+    def __init__(self, p, alphabet='ACGT'):
+        self.p = p
+        self.alphabet = alphabet
+        # Create map from alphabet characters to integers
+        self.amap = {}
+        for i in range(len(self.alphabet)):
+            self.amap[self.alphabet[i]] = i
+        # Make bad character rule table
+        self.bad_char = dense_bad_char_tab(p, self.amap)
+        # Create good suffix rule table
+        _, self.big_l, self.small_l_prime = good_suffix_table(p)
+    
+    def bad_character_rule(self, i, c):
+        """ Return # skips given by bad character rule at offset i """
+        assert c in self.amap
+        ci = self.amap[c]
+        assert i > (self.bad_char[i][ci]-1)
+        return i - (self.bad_char[i][ci]-1)
+    
+    def good_suffix_rule(self, i):
+        """ Given a mismatch at offset i, return amount to shift
+            as determined by (weak) good suffix rule. """
+        length = len(self.big_l)
+        assert i < length
+        if i == length - 1:
+            return 0
+        i += 1  # i points to leftmost matching position of P
+        if self.big_l[i] > 0:
+            return length - self.big_l[i]
+        return length - self.small_l_prime[i]
+    
+    def match_skip(self):
+        """ Return amount to shift in case where P matches T """
+        return len(self.small_l_prime) - self.small_l_prime[1]
